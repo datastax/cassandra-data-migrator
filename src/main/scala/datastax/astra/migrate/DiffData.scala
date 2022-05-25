468ea8a3f1c59eaaf32819816caae8c84111aafa
Vaishakh Narasimhareddy <vaishakh.narasimhareddy@datastax.com>
2022-05-25 13:14:56 -0700

    Changes to Diffdata to support cassandra to cassandra migration.

--- src/main/scala/datastax/astra/migrate/DiffData.scala
+++ src/main/scala/datastax/astra/migrate/DiffData.scala
@@ -28,23 +28,28 @@ object DiffData extends App {
   val sourceUsername = sc.getConf.get("spark.migrate.source.username")
   val sourcePassword = sc.getConf.get("spark.migrate.source.password")
   val sourceHost = sc.getConf.get("spark.migrate.source.host")
+  val sourceReadConsistencyLevel = sc.getConf.get("spark.cassandra.source.read.consistency.level","LOCAL_QUORUM")
 
-  val astraUsername = sc.getConf.get("spark.migrate.astra.username")
-  val astraPassword = sc.getConf.get("spark.migrate.astra.password")
+  val destinationHost =  sc.getConf.get("spark.migrate.destination.host")
+  val destintationUsername = sc.getConf.get("spark.migrate.destination.username")
+  val destinationPassword = sc.getConf.get("spark.migrate.destination.password")
+  val destinationReadConsistencyLevel = sc.getConf.get("spark.migrate.destination.read.consistency.level", "LOCAL_QUORUM")
 
   val astraScbPath = sc.getConf.get("spark.migrate.astra.scb")
+  val astraUsername = sc.getConf.get("spark.migrate.astra.username")
+  val astraPassword = sc.getConf.get("spark.migrate.astra.password")
+  val astraReadConsistencyLevel = sc.getConf.get("spark.cassandra.astra.read.consistency.level","LOCAL_QUORUM")
 
   val minPartition = new BigInteger(sc.getConf.get("spark.migrate.source.minPartition"))
   val maxPartition = new BigInteger(sc.getConf.get("spark.migrate.source.maxPartition"))
 
   val splitSize = sc.getConf.get("spark.migrate.splitSize","10000")
 
-  val sourceReadConsistencyLevel = sc.getConf.get("spark.cassandra.source.read.consistency.level","LOCAL_QUORUM")
-  val astraReadConsistencyLevel = sc.getConf.get("spark.cassandra.astra.read.consistency.level","LOCAL_QUORUM")
 
   println("Started Data Validation App")
 
   val isBeta = sc.getConf.get("spark.migrate.beta","false")
+  val isCassandraToCassandra = sc.getConf.get("spark.migrate.ctoc", "false")
 
   var sourceConnection = CassandraConnector(
     sc.getConf
@@ -63,14 +68,22 @@ object DiffData extends App {
     )
 
   }
-
-  val astraConnection = CassandraConnector(sc.getConf
+  var destinationConnection = CassandraConnector(sc.getConf
     .set("spark.cassandra.connection.config.cloud.path", astraScbPath)
     .set("spark.cassandra.auth.username", astraUsername)
     .set("spark.cassandra.auth.password", astraPassword)
     .set("spark.cassandra.input.consistency.level", astraReadConsistencyLevel))
 
-  diffTable(sourceConnection,astraConnection, minPartition, maxPartition)
+  if ("true".equals(isCassandraToCassandra)) {
+    destinationConnection = CassandraConnector(
+      sc.getConf
+        .set("spark.cassandra.connection.host", destinationHost)
+        .set("spark.cassandra.auth.username", destintationUsername)
+        .set("spark.cassandra.auth.password", destinationPassword)
+        .set("spark.cassandra.input.consistency.level", destinationReadConsistencyLevel))
+  }
+
+  diffTable(sourceConnection,destinationConnection, minPartition, maxPartition)
 
   exitSpark
 
