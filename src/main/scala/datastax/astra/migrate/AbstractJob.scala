81fde1460829ac9bde4629d6faf60c7b676f111b
Pravin Bhat <bhatpravin@gmail.com>
2022-11-15 07:46:29 -0500

    Fix connection issue when origin is non-Astra and target is Astra

--- src/main/scala/datastax/astra/migrate/AbstractJob.scala
+++ src/main/scala/datastax/astra/migrate/AbstractJob.scala
@@ -9,11 +9,24 @@ class AbstractJob extends BaseJob {
   abstractLogger.info("PARAM -- Split Size: " + splitSize)
   abstractLogger.info("PARAM -- Coverage Percent: " + coveragePercent)
 
-  var sourceConnection = getConnection(true, sourceIsAstra, sourceScbPath, sourceHost, sourceUsername, sourcePassword, sourceReadConsistencyLevel,
-    sourceTrustStorePath, sourceTrustStorePassword, sourceTrustStoreType, sourceKeyStorePath, sourceKeyStorePassword, sourceEnabledAlgorithms);
+  // Always connect to non-Astra source first. This is a workaround due to a CassandraConnector bug which gives
+  // preference to host in "spark.cassandra.connection.config.cloud.path" variable over "spark.cassandra.connection.host"
+  var sourceConnectionOpt =
+    if ("false".equals(sourceIsAstra))
+      Some(getConnection(true, sourceIsAstra, sourceScbPath, sourceHost, sourceUsername, sourcePassword, sourceReadConsistencyLevel,
+        sourceTrustStorePath, sourceTrustStorePassword, sourceTrustStoreType, sourceKeyStorePath, sourceKeyStorePassword, sourceEnabledAlgorithms))
+    else Option.empty;
+  var destinationConnectionOpt =
+    if ("false".equals(destinationIsAstra))
+      Some(getConnection(false, destinationIsAstra, destinationScbPath, destinationHost, destinationUsername, destinationPassword, destinationReadConsistencyLevel,
+        destinationTrustStorePath, destinationTrustStorePassword, destinationTrustStoreType, destinationKeyStorePath, destinationKeyStorePassword, destinationEnabledAlgorithms));
+    else Option.empty;
 
-  var destinationConnection = getConnection(false, destinationIsAstra, destinationScbPath, destinationHost, destinationUsername, destinationPassword, destinationReadConsistencyLevel,
-    destinationTrustStorePath, destinationTrustStorePassword, destinationTrustStoreType, destinationKeyStorePath, destinationKeyStorePassword, destinationEnabledAlgorithms);
+  // After connecting to non-Astra source (if present), now connect to Astra (if present)
+  var sourceConnection = sourceConnectionOpt.getOrElse(getConnection(true, sourceIsAstra, sourceScbPath, sourceHost, sourceUsername, sourcePassword, sourceReadConsistencyLevel,
+    sourceTrustStorePath, sourceTrustStorePassword, sourceTrustStoreType, sourceKeyStorePath, sourceKeyStorePassword, sourceEnabledAlgorithms));
+  var destinationConnection = destinationConnectionOpt.getOrElse(getConnection(false, destinationIsAstra, destinationScbPath, destinationHost, destinationUsername, destinationPassword, destinationReadConsistencyLevel,
+    destinationTrustStorePath, destinationTrustStorePassword, destinationTrustStoreType, destinationKeyStorePath, destinationKeyStorePassword, destinationEnabledAlgorithms));
 
   private def getConnection(isSource: Boolean, isAstra: String, scbPath: String, host: String, username: String, password: String, readConsistencyLevel: String,
                             trustStorePath: String, trustStorePassword: String, trustStoreType: String,
@@ -24,7 +37,7 @@ class AbstractJob extends BaseJob {
     }
 
     if ("true".equals(isAstra)) {
-      abstractLogger.info(connType + ": Connected to Astra using SCB: " + scbPath);
+      abstractLogger.info(connType + ": Connecting to Astra using SCB: " + scbPath);
 
       return CassandraConnector(sc
         .set("spark.cassandra.auth.username", username)
@@ -32,7 +45,7 @@ class AbstractJob extends BaseJob {
         .set("spark.cassandra.input.consistency.level", readConsistencyLevel)
         .set("spark.cassandra.connection.config.cloud.path", scbPath))
     } else if (null != trustStorePath && !trustStorePath.trim.isEmpty) {
-      abstractLogger.info(connType + ": Connected to Cassandra (or DSE) with SSL host: " + host);
+      abstractLogger.info(connType + ": Connecting to Cassandra (or DSE) with SSL host: " + host);
 
       // Use defaults when not provided
       var enabledAlgorithmsVar = enabledAlgorithms
@@ -55,7 +68,7 @@ class AbstractJob extends BaseJob {
         .set("spark.cassandra.connection.ssl.clientAuth.enabled", "true")
       )
     } else {
-      abstractLogger.info(connType + ": Connected to Cassandra (or DSE) host: " + host);
+      abstractLogger.info(connType + ": Connecting to Cassandra (or DSE) host: " + host);
 
       return CassandraConnector(sc.set("spark.cassandra.auth.username", username)
         .set("spark.cassandra.auth.password", password)
