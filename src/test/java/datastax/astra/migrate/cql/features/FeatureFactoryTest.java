cbfbfcec06c5e1147769fe6519f5c3beb94d5623
Phil Miesle <phil.miesle@datastax.com>
2023-03-26 15:52:08 +0100

    CDM-20 : establish Feature framework, re-implement OriginFilterCondition using framework

---
+++ src/test/java/datastax/astra/migrate/cql/features/FeatureFactoryTest.java
@@ -0,0 +1,13 @@
+package datastax.astra.migrate.cql.features;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FeatureFactoryTest {
+
+    @Test
+    public void knownButUnimplementedFeature() {
+        assertThrows(IllegalArgumentException.class, () -> FeatureFactory.getFeature(Featureset.TEST_UNIMPLEMENTED_FEATURE));
+    }
+}
