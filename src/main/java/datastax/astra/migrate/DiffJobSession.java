83e2fa55c27bb0c7c53962b2174e122b4d3d00a1
Pravin Bhat <bhatpravin@gmail.com>
2023-05-10 11:30:24 -0400

    CDM-58 Rethrow exceptions in DiffData so that partition-ranges with exceptions can be rerun similar to Migrate job

--- src/main/java/datastax/astra/migrate/DiffJobSession.java
+++ src/main/java/datastax/astra/migrate/DiffJobSession.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
@@ -107,8 +108,9 @@ public class DiffJobSession extends CopyJobSession {
             try {
                 Row targetRow = srcToTargetRowMap.get(srcRow).toCompletableFuture().get().one();
                 diff(srcRow, targetRow);
-            } catch (Exception e) {
+            } catch (ExecutionException | InterruptedException e) {
                 logger.error("Could not perform diff for Key: {}", getKey(srcRow, tableInfo), e);
+                throw new RuntimeException(e);
             }
         }
         srcToTargetRowMap.clear();
