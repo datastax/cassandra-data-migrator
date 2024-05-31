697475090b8ab6b5b143ce5ad399b0daa7cd7d05
Pravin Bhat <bhatpravin@gmail.com>
2024-05-31 17:04:15 -0400

    Remove common lang from project (use lang3)

--- src/main/java/com/datastax/cdm/schema/CqlTable.java
+++ src/main/java/com/datastax/cdm/schema/CqlTable.java
@@ -26,7 +26,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
