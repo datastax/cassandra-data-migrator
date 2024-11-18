794422573d43b57e41f8d38c750156345d8069e9
Pravin Bhat <pravin.bhat@datastax.com>
2024-11-18 07:53:45 -0500

    Improved metrics output consistency and removed references to Thread and Global variables. (#329)

--- src/test/java/com/datastax/cdm/job/JobCounterTest.java
+++ src/test/java/com/datastax/cdm/job/JobCounterTest.java
@@ -15,18 +15,11 @@
  */
 package com.datastax.cdm.job;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
 import com.datastax.cdm.feature.TrackRun;
 import com.datastax.cdm.job.IJobSessionFactory.JobType;
@@ -44,22 +37,35 @@ public class JobCounterTest {
     }
 
     @Test
-    public void testThreadIncrement() {
-        jobCounter.threadIncrement(JobCounter.CounterType.READ, 5);
+    public void interimIncrement() {
+        jobCounter.increment(JobCounter.CounterType.READ, 5);
+        assertEquals(5, jobCounter.getCount(JobCounter.CounterType.READ, true));
+        assertEquals(0, jobCounter.getCount(JobCounter.CounterType.READ));
+    }
+
+    @Test
+    public void increment() {
+        jobCounter.increment(JobCounter.CounterType.READ, 5);
+        jobCounter.flush();
+        assertEquals(0, jobCounter.getCount(JobCounter.CounterType.READ, true));
         assertEquals(5, jobCounter.getCount(JobCounter.CounterType.READ));
     }
 
     @Test
-    public void testGlobalIncrement() {
-        jobCounter.threadIncrement(JobCounter.CounterType.READ, 5);
-        jobCounter.globalIncrement();
-        assertEquals(5, jobCounter.getCount(JobCounter.CounterType.READ, true));
+    public void incrementByOne() {
+        jobCounter.increment(JobCounter.CounterType.READ, 5);
+        jobCounter.increment(JobCounter.CounterType.READ);
+        assertEquals(6, jobCounter.getCount(JobCounter.CounterType.READ, true));
+        assertEquals(0, jobCounter.getCount(JobCounter.CounterType.READ));
+        jobCounter.flush();
+        assertEquals(6, jobCounter.getCount(JobCounter.CounterType.READ));
     }
 
     @Test
-    public void testThreadResetForSpecificType() {
-        jobCounter.threadIncrement(JobCounter.CounterType.READ, 5);
-        jobCounter.threadReset(JobCounter.CounterType.READ);
+    public void resetForSpecificType() {
+        jobCounter.increment(JobCounter.CounterType.READ, 5);
+        jobCounter.reset(JobCounter.CounterType.READ);
+        assertEquals(0, jobCounter.getCount(JobCounter.CounterType.READ, true));
         assertEquals(0, jobCounter.getCount(JobCounter.CounterType.READ));
     }
 
@@ -67,23 +73,7 @@ public class JobCounterTest {
     public void testUnregisteredCounterType() {
         JobCounter localJobCounter = new JobCounter(JobType.MIGRATE);
         assertThrows(IllegalArgumentException.class,
-                () -> localJobCounter.threadIncrement(JobCounter.CounterType.CORRECTED_MISMATCH, 5));
-    }
-
-    @Test
-    public void testPrintProgressForGlobalAndThread() {
-        jobCounter.threadIncrement(JobCounter.CounterType.READ, 11);
-        jobCounter.globalIncrement();
-        // You may use mocking to capture logger outputs
-        jobCounter.printMetrics(0, null);
-    }
-
-    @Test
-    public void testPrintFinal() {
-        jobCounter.threadIncrement(JobCounter.CounterType.READ, 5);
-        jobCounter.globalIncrement();
-        // You may use mocking to capture logger outputs
-        jobCounter.printMetrics(0, null);
+                () -> localJobCounter.increment(JobCounter.CounterType.CORRECTED_MISMATCH, 5));
     }
 
     @Captor
@@ -92,35 +82,83 @@ public class JobCounterTest {
     @Captor
     private ArgumentCaptor<String> trackRunInfoCaptor;
 
-    // @Test
-    // public void testPrintFinalWithRunTracking() {
-    // jobCounter = new JobCounter(JobType.VALIDATE);
-    //
-    // String expected = "Read: 5; Mismatch: 0; Corrected Mismatch: 0; Missing: 7; Corrected Missing: 7; Valid: 0;
-    // Skipped: 0; Error: 72";
-    // jobCounter.threadIncrement(JobCounter.CounterType.READ, 5);
-    // jobCounter.threadIncrement(JobCounter.CounterType.CORRECTED_MISSING, 7);
-    // jobCounter.threadIncrement(JobCounter.CounterType.ERROR, 72);
-    // jobCounter.threadIncrement(JobCounter.CounterType.MISSING, 7);
-    // jobCounter.globalIncrement();
-    // // You may use mocking to capture logger outputs
-    // jobCounter.printMetrics(0, trackRun);
-    // Mockito.verify(trackRun).endCdmRun(trackRunInfoCaptorLong.capture(), trackRunInfoCaptor.capture());
-    // assertEquals(expected, trackRunInfoCaptor.getValue());
-    // }
-
     @Test
-    public void testGetCountGlobal() {
-        jobCounter.threadIncrement(JobCounter.CounterType.READ, 5);
-        jobCounter.globalIncrement();
-        assertEquals(5, jobCounter.getCount(JobCounter.CounterType.READ, true));
+    public void printMetricsMigrate() {
+        jobCounter = new JobCounter(JobType.MIGRATE);
+
+        String expected = "Read: 10; Write: 7; Skipped: 1; Error: 2";
+        jobCounter.increment(JobCounter.CounterType.READ, 10);
+        jobCounter.increment(JobCounter.CounterType.WRITE, 7);
+        jobCounter.increment(JobCounter.CounterType.ERROR, 2);
+        jobCounter.increment(JobCounter.CounterType.SKIPPED, 1);
+        jobCounter.increment(JobCounter.CounterType.UNFLUSHED, 3);
+        jobCounter.flush();
+        // You may use mocking to capture logger outputs
+        jobCounter.printMetrics(0, trackRun);
+        Mockito.verify(trackRun).endCdmRun(trackRunInfoCaptorLong.capture(), trackRunInfoCaptor.capture());
+        assertEquals(expected, trackRunInfoCaptor.getValue());
     }
 
     @Test
-    public void threadIncrementByOne() {
-        jobCounter.threadIncrement(JobCounter.CounterType.READ, 5);
-        jobCounter.threadIncrement(JobCounter.CounterType.READ);
-        assertEquals(6, jobCounter.getCount(JobCounter.CounterType.READ));
+    public void printMetricsValidate() {
+        jobCounter = new JobCounter(JobType.VALIDATE);
+
+        String expected = "Read: 5; Mismatch: 0; Corrected Mismatch: 0; Missing: 7; Corrected Missing: 7; Valid: 0; Skipped: 0; Error: 72";
+        jobCounter.increment(JobCounter.CounterType.READ, 5);
+        jobCounter.increment(JobCounter.CounterType.CORRECTED_MISSING, 7);
+        jobCounter.increment(JobCounter.CounterType.ERROR, 72);
+        jobCounter.increment(JobCounter.CounterType.MISSING, 7);
+        jobCounter.flush();
+        // You may use mocking to capture logger outputs
+        jobCounter.printMetrics(0, trackRun);
+        Mockito.verify(trackRun).endCdmRun(trackRunInfoCaptorLong.capture(), trackRunInfoCaptor.capture());
+        assertEquals(expected, trackRunInfoCaptor.getValue());
+    }
+
+    @Test
+    public void add() {
+        JobCounter localJobCounter = new JobCounter(JobType.MIGRATE);
+        localJobCounter.increment(JobCounter.CounterType.READ, 4);
+        localJobCounter.increment(JobCounter.CounterType.WRITE, 5);
+        localJobCounter.increment(JobCounter.CounterType.SKIPPED, 6);
+        localJobCounter.increment(JobCounter.CounterType.ERROR, 7);
+        localJobCounter.flush();
+        jobCounter.add(localJobCounter);
+
+        assertAll(() -> {
+            assertEquals(4, jobCounter.getCount(JobCounter.CounterType.READ));
+            assertEquals(5, jobCounter.getCount(JobCounter.CounterType.WRITE));
+            assertEquals(6, jobCounter.getCount(JobCounter.CounterType.SKIPPED));
+            assertEquals(7, jobCounter.getCount(JobCounter.CounterType.ERROR));
+            assertEquals(0, jobCounter.getCount(JobCounter.CounterType.UNFLUSHED));
+        });
+    }
+
+    @Test
+    public void reset() {
+        jobCounter.reset();
+
+        assertAll(() -> {
+            assertEquals(0, jobCounter.getCount(JobCounter.CounterType.READ));
+            assertEquals(0, jobCounter.getCount(JobCounter.CounterType.WRITE));
+            assertEquals(0, jobCounter.getCount(JobCounter.CounterType.SKIPPED));
+            assertEquals(0, jobCounter.getCount(JobCounter.CounterType.ERROR));
+            assertEquals(0, jobCounter.getCount(JobCounter.CounterType.UNFLUSHED));
+        });
+    }
+
+    @Test
+    public void isZero() {
+        assertTrue(jobCounter.isZero());
+
+        jobCounter.increment(JobCounter.CounterType.READ, 5);
+        assertFalse(jobCounter.isZero());
+    }
+
+    @Test
+    public void initGuardrail() {
+        JobCounter localJobCounter = new JobCounter(JobType.GUARDRAIL);
+        assertThrows(IllegalArgumentException.class, () -> localJobCounter.increment(JobCounter.CounterType.WRITE, 5));
     }
 
 }
